282a0410d3b3fce87d9cbe0201e179ba8d661bf1
Volodymyr Havrylenko <vladimirvgavrilenko@gmail.com>
2024-11-20 17:53:25 +0200

    Solusion with flake8 debug

--- app/main.py
+++ app/main.py
@@ -1,39 +1,40 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"], 
-        "column": error["column_number"], 
-        "message": error["text"], 
-        "name": error["code"], 
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
         "source": "flake8"
-        }
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": 
-            [{"line": error["line_number"], 
-                "column": error["column_number"], 
-                "message": error["text"], 
-                "name": error["code"], 
-                "source": "flake8"} for error in errors
-            ],  
-        "path": file_path, 
+        "errors": [{"line": error["line_number"],
+                    "column": error["column_number"],
+                    "message": error["text"],
+                    "name": error["code"],
+                    "source": "flake8"} for error in errors],
+        "path": file_path,
         "status": "failed"
-        }
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return[
-        {"errors": [], "path": list(linter_report.keys())[i], "status": "passed"} if i == 0 else
+    return [
+        {"errors": [],
+         "path": list(linter_report.keys())[i],
+         "status": "passed"} if i == 0 else
         {"errors": [
-                {
-                    "line": error["line_number"], 
-                    "column": error["column_number"], 
-                    "message": error["text"], 
-                    "name": error["code"], 
-                    "source": "flake8"
-                } for error in linter_report[list(linter_report.keys())[i]]
-               ],
-        "path": list(linter_report.keys())[i],
-        "status": "failed"} for i in range(0, len(linter_report))
-        ]
+            {
+                "line": error["line_number"],
+                "column": error["column_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8"
+            } for error in linter_report[list(linter_report.keys())[i]]
+        ],
+            "path": list(linter_report.keys())[i],
+            "status": "failed"
+        } for i in range(0, len(linter_report))
+    ]
